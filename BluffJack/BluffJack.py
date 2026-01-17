ee9f2a4a11dc7f4ead42916b040af9889f8be131
ChaoticPoly <turtlefartnuggets@gmail.com>
2026-01-17 01:15:28 -0600

    arcana kind of works, debug button for a test arcana. no effects implimented yet. dropdowns do not support multiple of the same option so workaround for multiple of same tarots needs to be figured out

--- BluffJack/BluffJack.py
+++ BluffJack/BluffJack.py
@@ -6,6 +6,7 @@ import os
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
+COLOR = int(os.getenv('COLOR'), 16)
 
 intents = discord.Intents.default()
 intents.message_content = True  # also enable this in the Developer Portal
@@ -26,9 +27,12 @@ class game():
     p2deck = [] 
     p1hidden = []
     p2hidden = []
-    currenplayer = None  
+    currenplayer = player  
     goal = 21
     gameEmbed = discord.Embed
+    turn = 1
+    currenbet = 1
+    standcount = 0
     deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
     def draw(self, player, amount):
@@ -51,16 +55,22 @@ class game():
     def build_embed(self) -> discord.Embed:
         embed = discord.Embed(
             title="Bluff Challenge",
-            description=str(self.deck),
-            color=discord.Color.blurple()
+            description=str(f"Bust Limit: {self.goal}"),
+            color=COLOR
         )
-        embed.add_field(name=self.p1.player, value=self.p1.hand, inline=True)
-        embed.add_field(name=self.p2.player, value=self.p2.hand, inline=True)
+        embed.add_field(name=f"{self.p1.player} HP {self.p1.health}", value=self.p1.hand, inline=True)
+        embed.add_field(name=f"{self.p2.player} HP {self.p2.health}", value=self.p2.hand, inline=True)
         embed.add_field(name="\u000b", value="\u000b", inline=False)
         embed.add_field(name="Hand", value=self.p1.hidden, inline=True)
         embed.add_field(name="Hand", value=self.p2.hidden, inline=True)
-        embed.set_footer(text=f"{self.p1.player} vs {self.p2.player}")
+        embed.set_footer(text=f"Turn {self.turn} | Bet {self.currenbet} | {self.currenplayer.player}'s turn")
         return embed
+    def fool(self):
+        for i in self.currenplayer.hand:
+            self.deck.append(i)
+        self.currenplayer.hand = []
+        self.currenplayer.hidden = []
+        self.currenplayer.sum()
     def __init__(self, play1, play2, starthp): # health, bet
         self.starthp = starthp
         self.p1, self.p2 = player(play1, starthp), player(play2, starthp)
@@ -68,32 +78,59 @@ class game():
         print(self.p1)
         for i in players:
             self.draw(i, 2)
-        self.gameEmbed = self.build_embed()
         self.currenplayer = self.p1
         self.otherplayer = self.p2
-
-
-#instancex = game("me", "you")
-#playerx = player(instancex.p1)
-#playery = player(instancex.p2)
-#playerx.health = instancex.p1health
+        self.gameEmbed = self.build_embed()
 
 class PlayView(discord.ui.View):
     def __init__(self, board):
         super().__init__(timeout=None)
         self.board = board 
     @discord.ui.button(label="Hit", style=discord.ButtonStyle.green)
-    async def on_accept_click(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def on_hit_click(self, interaction: discord.Interaction, button: discord.ui.Button):
         self.board.draw(self.board.currenplayer, 1)
+        self.board.standcount = 0
         await interaction.response.edit_message(embed=self.board.build_embed(), view=self)
     @discord.ui.button(label="Stand", style=discord.ButtonStyle.red)
-    async def on_deny_click(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def on_stand_click(self, interaction: discord.Interaction, button: discord.ui.Button):
         self.board.currenplayer, self.board.otherplayer = self.board.otherplayer, self.board.currenplayer
+        self.board.turn += 1
+        self.board.standcount += 1
+        if self.board.standcount >=2:
+            #resolve round
+            pass
         await interaction.response.edit_message(embed=self.board.build_embed(), view=self)
+    @discord.ui.button(label="Arcana", style=discord.ButtonStyle.blurple)
+    async def on_arcana_click(self, interaction: discord.Interaction, button: discord.ui.Button):
+        view = ArcanaView(self.board.currenplayer.tarot)
+        await interaction.response.send_message(embed=None, view=view, ephemeral=True)
+    @discord.ui.button(label="Debug", style=discord.ButtonStyle.blurple)
+    async def on_debug_click(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.board.currenplayer.tarot.append(("Test Arcana","A powerful spell."))
+        self.board.currenplayer.tarot.append(("Test Arcana2","A weak spell."))
+        await interaction.response.send_message(content="spawning test arcana",ephemeral=True)
+
+class ArcanaView(discord.ui.View):  
+    def build_dropdown(self, options: list[tuple[str, str]]):
+        dropdown = discord.ui.Select(
+            placeholder="Choose an option...",
+            options=[
+                discord.SelectOption(label=label, description=desc)
+                for label, desc in options
+            ],
+        )
+        return dropdown
+    def __init__(self, tarotinv):
+        super().__init__(timeout=None)
+        dropdown = self.build_dropdown(tarotinv)
+        dropdown.callback = self.select_callback
+        self.add_item(dropdown)
+        
+    async def select_callback(self, select, interaction): # the function called when the user is done selecting options
+        await interaction.response.send_message(f"Invoking {select.values[0]}.")
+    
 
 class AcceptView(discord.ui.View):
-    # This button will have a red style and the label "Click Me!"
-
     def __init__(self, play1, play2):
         super().__init__(timeout=None)
         self.player1, self.player2 = play1, play2
@@ -104,10 +141,14 @@ class AcceptView(discord.ui.View):
         board = game(self.player1, self.player2, 7)
         playbuttons = PlayView(board)
         await interaction.response.edit_message(embed=board.gameEmbed, view=playbuttons)
+
     @discord.ui.button(label="Deny", style=discord.ButtonStyle.red)
     async def on_deny_click(self, interaction: discord.Interaction, button: discord.ui.Button):
-        """Callback fself.player1,self.player2unction for the button click."""
-        # Acknowledge the interaction and edit the mesboard.gameEmbed        await interaction.response.edit_message(content="Button clicked!", embed=None, view=None)
+        await interaction.response.edit_message(content=None, embed=discord.Embed(
+            title="Bluff Challenge",
+            description=str(f"Challenge Denied"),
+            color=0x363636
+        ), view=None)
 
       
 @bot.group(invoke_without_command=True)
@@ -121,7 +162,7 @@ async def duel(ctx, target: discord.Member):
     embed = discord.Embed(
     title="Bluff Challenge",
     description=f"{author.mention} has challenged {target.mention}!",
-    color=discord.Color.blurple()
+    color=COLOR
 )
     embed.set_footer(text=f"{author} vs {target}")
     accept = AcceptView(author,target)
