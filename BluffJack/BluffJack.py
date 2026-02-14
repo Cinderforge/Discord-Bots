ece5c192732b1ce3f8321c76bd812c01a0e7becf
ChaoticPoly <turtlefartnuggets@gmail.com>
2026-02-14 14:59:34 -0600

    challenge timeout, user validation, new draw functions for hit and begin round added. fixed shared deck issue

--- BluffJack/BluffJack.py
+++ BluffJack/BluffJack.py
@@ -17,6 +17,7 @@ class player():
     def __init__(self, player, health): 
         self.player = player
         self.health = health
+        self.id = player.id
         self.hand, self.hidden, self.tarot, self.psum, self.hsum= [], [], [], 0, 0
     def sum(self):
         self.psum = sum(self.hand)
@@ -24,47 +25,29 @@ class player():
 
 
 class game():
-    currenplayer = player  
-    goal = 21
-    gameEmbed = discord.Embed
-    turn = 1
-    currenbet = 1
-    standcount = 0
-    deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-
-    def draw(self, player, amount):
-        if player.psum > self.goal:
-            print("Busted! You may not draw a card")
-            print(self.goal)
-        else: 
-            if len(self.deck) > 0:
-                for _ in range(amount):
-                    drawn = random.choice(self.deck)
-                    player.hand.append(drawn)
-                    if len(player.hidden) > 1:
-                        player.hidden.append(0)
-                    else:
-                        player.hidden.append(drawn)
-                    self.deck.remove(drawn)
-            else:
-                print("Empty deck!")
-        player.sum()
     def build_embed(self) -> discord.Embed:
+        
         embed = discord.Embed(
             title="Bluff Challenge",
             description=str(f"Bust Limit: {self.goal}"),
             color=COLOR
         )
-        embed.add_field(name=f"{self.p1.player} HP {self.p1.health}", value=self.p1.hand, inline=True)
-        embed.add_field(name=f"{self.p2.player} HP {self.p2.health}", value=self.p2.hand, inline=True)
+        embed.add_field(name=f"{self.p1.player}", value=f"HP {self.p1.health}", inline=True)
+        embed.add_field(name=f"{self.p2.player}", value=f"HP {self.p2.health}", inline=True)
         embed.add_field(name="\u000b", value="\u000b", inline=False)
         embed.add_field(name="Hand", value=self.p1.hidden, inline=True)
         embed.add_field(name="Hand", value=self.p2.hidden, inline=True)
         embed.set_footer(text=f"Turn {self.turn} | Bet {self.currenbet} | {self.currenplayer.player}'s turn")
         return embed
-
+    
+    def beginDraw(self, player):
+        for _ in range(2):
+            drawn = random.choice(self.deck)
+            player.hand.append(drawn)
+            player.hidden.append(0)
+            self.deck.remove(drawn)
+        
     def endround(self):
-        a, b = self.currenplayer.psum, self.otherplayer.psum
         print(f"{self.currenplayer.player} other {self.otherplayer.player}")
         print(f"{self.currenplayer.player}'s Score: {self.currenplayer.psum} {self.otherplayer.player}'s Score: {self.otherplayer.psum}")
         winner = None
@@ -91,7 +74,8 @@ class game():
         self.standcount = 0
         self.currenbet += 1
         self.turn += 1
-
+    def gameover():
+        pass
     def fool(self):
         for i in self.currenplayer.hand:
             self.deck.append(i)
@@ -101,25 +85,74 @@ class game():
         self.currenplayer.draw(self.currenplayer, 2)
         
     def __init__(self, play1, play2, starthp): # health, bet
+        self.currenplayer = player  
+        self.goal = 21
+        self.gameEmbed = discord.Embed
+        self.turn = 1
+        self.currenbet = 1
+        self.standcount = 0
+        self.deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
         self.starthp = starthp
         self.p1, self.p2 = player(play1, self.starthp), player(play2, self.starthp)
         players = [self.p1, self.p2]
         print(self.p1)
         for i in players:
-            self.draw(i, 2)
+            self.beginDraw(i)
         self.currenplayer = self.p1
         self.otherplayer = self.p2
         self.gameEmbed = self.build_embed()
 
 class PlayView(discord.ui.View):
-    def __init__(self, board):
-        super().__init__(timeout=None)
+    def __init__(self, board, timeout: int = None):
+        super().__init__(timeout=timeout)
         self.board = board 
+
+    async def on_timeout(self):
+        self.board.currenplayer.hand.append('X')
+        self.board.currenplayer, self.board.otherplayer = (
+            self.board.otherplayer,
+            self.board.currenplayer,
+        )
+        
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        if interaction.user.id != self.board.currenplayer.id:
+            embed = discord.Embed(
+                title="Bluff Message",
+                description=str(f"It is not your turn!"),
+                color=COLOR
+            )
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+            return False  
+        return True  
+    
     @discord.ui.button(label="Hit", style=discord.ButtonStyle.green)
     async def on_hit_click(self, interaction: discord.Interaction, button: discord.ui.Button):
-        self.board.draw(self.board.currenplayer, 1)
-        self.board.standcount = 0
-        await interaction.response.edit_message(embed=self.board.build_embed(), view=self)
+        if self.board.currenplayer.psum > self.board.goal:
+            embed = discord.Embed(
+                title="Bluff Message",
+                description=str("Bro you busted."),
+                
+                color=COLOR
+            )
+            embed.set_image(url="https://media.tenor.com/I50TI2DmFXIAAAAM/yuji-stare-yuji-itadori.gif")
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+        else:
+            if len(self.board.deck) > 0:
+                drawn = random.choice(self.board.deck)
+                self.board.currenplayer.hand.append(drawn)
+                self.board.currenplayer.hidden.append(drawn)
+                self.board.deck.remove(drawn)
+                self.board.standcount = 0
+                await interaction.response.edit_message(embed=self.board.build_embed(), view=self)
+            else:
+                embed = discord.Embed(
+                title="Bluff Message",
+                description=str(f"Empty deck!"),
+                color=COLOR
+            )
+                await interaction.response.send_message(embed=embed, ephemeral=True)
+            self.board.currenplayer.sum()
+    
     @discord.ui.button(label="Stand", style=discord.ButtonStyle.red)
     async def on_stand_click(self, interaction: discord.Interaction, button: discord.ui.Button):
         self.board.currenplayer, self.board.otherplayer = self.board.otherplayer, self.board.currenplayer
@@ -127,11 +160,17 @@ class PlayView(discord.ui.View):
         if self.board.standcount >=2:
             self.board.endround()
         await interaction.response.edit_message(embed=self.board.build_embed(), view=self)
+    
     @discord.ui.button(label="Arcana", style=discord.ButtonStyle.blurple)
     async def on_arcana_click(self, interaction: discord.Interaction, button: discord.ui.Button):
         view = ArcanaView(self.board.currenplayer.tarot)
         self.board.currenplayer.tarot = view.newlist
         await interaction.response.send_message(embed=None, view=view, ephemeral=True)
+    
+    @discord.ui.button(label="Hand", style=discord.ButtonStyle.gray)
+    async def on_hand_click(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message(content=f"Your hand: {self.board.currenplayer.hand}\nHidden: {self.board.currenplayer.hidden}\nTotal: {self.board.currenplayer.psum}",ephemeral=True)
+    
     @discord.ui.button(label="Debug", style=discord.ButtonStyle.blurple)
     async def on_debug_click(self, interaction: discord.Interaction, button: discord.ui.Button):
         self.board.currenplayer.tarot.append(["Test Arcana","A powerful spell.",3])
@@ -172,15 +211,33 @@ class ArcanaView(discord.ui.View):
 
 class AcceptView(discord.ui.View):
     def __init__(self, play1, play2):
-        super().__init__(timeout=None)
+        super().__init__(timeout=30.0)
         self.player1, self.player2 = play1, play2
-        print(self.player1)
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        if interaction.user.id != self.player2.id:
+            embed = discord.Embed(
+                title="Bluff Message",
+                description=str(f"Not you lil bro..."),
+                color=COLOR
+            )
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+            return False  
+        return True
+    
+    async def on_timeout(self):
+        if self.message:
+            await self.message.edit(content=None, embed=discord.Embed(
+                title="Bluff Challenge",
+                description=str(f"Challenge Timed Out"),
+                color=0x363636
+                ), view=None)
 
     @discord.ui.button(label="Accept", style=discord.ButtonStyle.blurple)
     async def on_accept_click(self, interaction: discord.Interaction, button: discord.ui.Button):
         board = game(self.player1, self.player2, 7)
         playbuttons = PlayView(board)
         await interaction.response.edit_message(embed=board.gameEmbed, view=playbuttons)
+        self.stop()
 
     @discord.ui.button(label="Deny", style=discord.ButtonStyle.red)
     async def on_deny_click(self, interaction: discord.Interaction, button: discord.ui.Button):
@@ -189,6 +246,7 @@ class AcceptView(discord.ui.View):
             description=str(f"Challenge Denied"),
             color=0x363636
         ), view=None)
+        self.stop()
 
       
 @bot.group(invoke_without_command=True)
@@ -206,6 +264,6 @@ async def duel(ctx, target: discord.Member):
 )
     embed.set_footer(text=f"{author} vs {target}")
     accept = AcceptView(author,target)
-    await ctx.send(embed=embed, view=accept) 
+    accept.message = await ctx.send(embed=embed, view=accept)
 
 bot.run(TOKEN)
